ada5163e455d4fc400e49d66dbaec0e7f79e5324
Camilla <camillefranzen@gmail.com>
2025-05-02 14:59:32 +0200

    Enhance header component: add container and logo structure, update text content, and apply background styles

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -1,18 +1,21 @@
-"use client";
+
 import Image from "next/image";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <Image src="/images/logo.png" alt="Logo" width={100} height={50} />
-      </div>
-      <div className="header__text">
-        <h1 className="header__title">Välkommen till vår hemsida</h1>
-        <p className="header__subtitle">
-          Din källa för information och inspiration
-        </p>
-      </div>
-    </header>
-  );
+   return (
+     <header className="header">
+       <div className="header__container">
+        <div className="header__logo">
+            <Image src={"/images/logo-placeholder.jpg"} alt="Logo" width={100} height={100} />
+        </div>
+        
+         <div className="header__text">
+           <h1 className="header__title">Välkommen till vår förening</h1>
+           <p className="header__subtitle">
+             Här hittar du allt om boende, kontakt och miljö.
+           </p>
+         </div>
+       </div>
+     </header>
+   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,15 @@
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import "./_styles/main.scss";
+import Header from "./components/layout/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="sv">
       <body>
+        <Header />
         <Navbar />
-        <main>{children}</main>
+        <main className="site-wrap">{children}</main>
         <Footer />
       </body>
     </html>
